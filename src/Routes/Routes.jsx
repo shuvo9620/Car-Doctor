4c2f3d6f9014a5152244c3896bfa07d1a6db9262
shuvo9620 <smahfuzur0@gmail.com>
2023-05-16 13:06:35 +0600

    navbar added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
@@ -8,7 +9,10 @@ const router = createBrowserRouter([
       children: [
         {
             path: '/',
-            element: 
+            element: <Home></Home>
+        },
+        {
+            
         }
       ]
     },
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2>home thus is</h2>
+        </div>
+    );
+};
+
+export default Home;
